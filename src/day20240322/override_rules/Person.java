cf7ef2320f48845b386521482e64353b54fa9776
zxhhhxz1221 <1305894626@qq.com>
2024-03-22 19:46:05 +0800

    override

---
+++ src/day20240322/override_rules/Person.java
@@ -0,0 +1,33 @@
+package day20240322.override_rules;
+
+/**
+ * @Author: XinhaoZheng
+ * @CreateTime: 2024-03-22  19:38
+ * @Description: TODO
+ */
+public class Person {
+
+    public String name;
+    public String cardId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(String cardId) {
+        this.cardId = cardId;
+    }
+
+    public void eat(){
+        System.out.println("吃吃吃。。。。。");
+    }
+
+}
---
+++ src/day20240322/override_rules/Student.java
@@ -0,0 +1,24 @@
+package day20240322.override_rules;
+
+/**
+ * @Author: XinhaoZheng
+ * @CreateTime: 2024-03-22  19:39
+ * @Description: TODO
+ */
+public class Student extends Person{
+
+    public String studentCardId;
+
+    public String getStudentCardId() {
+        return studentCardId;
+    }
+
+    public void setStudentCardId(String studentCardId) {
+        this.studentCardId = studentCardId;
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("吃食堂。。。");
+    }
+}
