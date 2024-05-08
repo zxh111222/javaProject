5a4d7051a6342138a118fe997ddc282cde7d54c0
zxhhhxz1221 <1305894626@qq.com>
2024-05-08 20:55:04 +0800

    练习 - 直接用 JDK

--- src/day20240508/practice/Practice1.java
+++ src/day20240508/practice/Practice1.java
@@ -3,10 +3,13 @@ package day20240508.practice;
 public class Practice1 {
     public static void main(String[] args) {
         // 需求：如果计算出这个算法的运行时间
+        long startTime = System.currentTimeMillis();
         int sum = 0;
         for (int i = 0; i <= 100; i++) {
             sum += i;
         }
         System.out.println(sum);
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime-startTime);
     }
 }
