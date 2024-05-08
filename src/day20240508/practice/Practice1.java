c72087c1e17b38dafb4b96bfc47f007af48056d4
zxhhhxz1221 <1305894626@qq.com>
2024-05-08 20:48:01 +0800

    练习 - 新需求：如果计算出这个算法的运行时间

---
+++ src/day20240508/practice/Practice1.java
@@ -0,0 +1,12 @@
+package day20240508.practice;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        // 需求：如果计算出这个算法的运行时间
+        int sum = 0;
+        for (int i = 0; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
