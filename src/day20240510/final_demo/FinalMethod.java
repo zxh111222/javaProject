aeee661a32e6881cbdacb2732961b66fe6c29423
zxhhhxz1221 <1305894626@qq.com>
2024-05-10 19:58:18 +0800

    final - final修饰的方法不能被重写

---
+++ src/day20240510/final_demo/FinalMethod.java
@@ -0,0 +1,14 @@
+package day20240510.final_demo;
+
+public class FinalMethod {
+    public final void finalMethod() {
+
+    }
+}
+
+//class SubFinalMethod extends FinalMethod {
+////'finalMethod()' cannot override 'finalMethod()' in 'day20240510.final_demo.FinalMethod'; overridden method is fina
+//    public void finalMethod() {
+//        super.finalMethod();
+//    }
+//}
--- src/day20240510/final_demo/SubFinalClass.java
+++
@@ -1,5 +0,0 @@
-package day20240510.final_demo;
-
-// Cannot inherit from final 'day20240510.final_demo.FinalClass'
-//public class SubFinalClass extends FinalClass {
-//}
