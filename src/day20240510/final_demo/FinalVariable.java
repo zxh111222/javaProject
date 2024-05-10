fad0adaf18f1cb18b98fcc5851f6e46d84919015
zxhhhxz1221 <1305894626@qq.com>
2024-05-10 20:20:26 +0800

    final - 验证final修饰不同类型对象 - 基础代码

---
+++ src/day20240510/final_demo/FinalVariable.java
@@ -0,0 +1,29 @@
+package day20240510.final_demo;
+
+public class FinalVariable {
+
+
+    // Instance Variables(Non-Static Fileds)
+    public String myName = "zxh";
+
+    // Class Variables(Static Fileds)
+    public static String id = "123456789";
+
+    // Local Variables
+    public void method1() {
+        int number = 100;
+        System.out.println(number);
+    }
+
+    //Parameters
+    public void method2(int number) {
+        number = 200;
+        System.out.println(number);
+    }
+
+
+
+}
+
+
+
---
+++ src/day20240510/final_demo/Test.java
@@ -0,0 +1,11 @@
+package day20240510.final_demo;
+
+public class Test {
+
+    public static void main(String[] args) {
+        FinalVariable finalVariable = new FinalVariable();
+        finalVariable.myName = "zzz";
+
+    }
+
+}
