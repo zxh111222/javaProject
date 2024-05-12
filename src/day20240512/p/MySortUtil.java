ab76ea5d3d16a0027e3f196c47623586a5e35185
zxhhhxz1221 <1305894626@qq.com>
2024-05-12 10:09:48 +0800

    MySortUtil - 轮廓（以终为始）

---
+++ src/day20240512/p/MySortUtil.java
@@ -0,0 +1,12 @@
+package day20240512.p;
+
+public class MySortUtil {
+
+    public static void print(int[] arr){
+
+    }
+
+    public static void sort(int[] arr) {
+
+    }
+}
---
+++ src/day20240512/p/Test.java
@@ -0,0 +1,9 @@
+package day20240512.p;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] arr = new int[] {4, 2, 7, 1, 9, 5, 3, 8, 6};
+        MySortUtil.sort(arr);
+        MySortUtil.print(arr);
+    }
+}
