973d17ca7c23c3df72da709c9764cdd161391b36
zxhhhxz1221 <1305894626@qq.com>
2024-05-12 10:15:26 +0800

    MySortUtil - 执行排序int[]

--- src/day20240512/p/MySortUtil.java
+++ src/day20240512/p/MySortUtil.java
@@ -2,11 +2,22 @@ package day20240512.p;
 
 public class MySortUtil {
 
-    public static void print(int[] arr){
 
-    }
 
     public static void sort(int[] arr) {
-
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < (arr.length - 1) - i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
     }
 }
