8378328f41ee49d2fb7006fc5a60d6de24761bdb
zxhhhxz1221 <1305894626@qq.com>
2024-05-15 20:23:06 +0800

    回顾 char 类型以及对应的数值

---
+++ src/day20240515/p/Test2.java
@@ -0,0 +1,19 @@
+package day20240515.p;
+
+public class Test2 {
+    public static void main(String[] args) {
+        char a = 'A';
+        char b = 'B';
+        char c = 'C';
+        char d = 'D';
+        char e = 'E';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+
+    }
+
+}
