235c23d01287e7bd5785974c86d2d8034ad1e19f
zxhhhxz1221 <1305894626@qq.com>
2024-05-15 20:48:05 +0800

    新增 MyComparator 接口，让所有的比较器都实现它

---
+++ src/day20240515/p/MyComparator.java
@@ -0,0 +1,6 @@
+package day20240515.p;
+
+public interface MyComparator {
+    int compare(MyComparator o1, MyComparator o2);
+
+}
---
+++ src/day20240515/p/StudentAgeCompare.java
@@ -0,0 +1,8 @@
+package day20240515.p;
+
+public class StudentAgeCompare implements MyComparator {
+    @Override
+    public int compare(MyComparator o1, MyComparator o2) {
+        return 0;
+    }
+}
