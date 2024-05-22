15ce6d4909b42b29a7b91d3dae24b589a2eb0fd9
zxhhhxz1221 <1305894626@qq.com>
2024-05-22 21:30:26 +0800

    `泛型`练习 - v5 MyMap 完整测试

--- src/day20240522/p2/Test.java
+++ src/day20240522/p2/Test.java
@@ -1,17 +1,21 @@
 package day20240522.p2;
 
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
-        MyMap a = new MyMap("实训室8", "4台风扇");
+        MyMap<String,String> a = new MyMap<>("实训室8", "4台风扇");
 
-        MyMap b = new MyMap();
+        MyMap<String,String> b = new MyMap<>();
         b.set("实训室3", "3台风扇");
         b.get("实训室3");
         System.out.println(b.get("实训室3"));
 
-        MyMap c = new MyMap("实训室1", new Student[]{new Student("学生1"), new Student("学生2")});
+        System.out.println("=== === ===");
 
-
+        MyMap<String, Student[]> c = new MyMap<>("实训室1", new Student[]{new Student("学生1"), new Student("学生2")});
+        c.set("实训9",new Student[]{new Student("a"),new Student("b")});
+        System.out.println(Arrays.toString(c.get("实训9")));
 
 
     }
