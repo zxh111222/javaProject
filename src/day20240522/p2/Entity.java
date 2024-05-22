be0637a3015da348b1489811330f628b205f04f9
zxhhhxz1221 <1305894626@qq.com>
2024-05-22 20:48:27 +0800

    `泛型`练习 - v2 Entity 支持第二种创建实例方式
    Entity b = new Entity();
    b.set("实训室3", "3台风扇");
    b.get("实训室3");

--- src/day20240522/p2/Entity.java
+++ src/day20240522/p2/Entity.java
@@ -4,26 +4,21 @@ public class Entity {
     public String classRoom;
     public String fan;
 
-
+    public Entity() {
+    }
 
     public Entity(String classRoom, String fan) {
         this.classRoom = classRoom;
         this.fan = fan;
     }
 
-    public String getClassRoom() {
-        return classRoom;
-    }
-
-    public void setClassRoom(String classRoom) {
+    public void set(String classRoom, String fan) {
         this.classRoom = classRoom;
-    }
-
-    public String getFan() {
-        return fan;
-    }
-
-    public void setFan(String fan) {
         this.fan = fan;
     }
+
+    public void get(String classRoom){
+        this.fan = fan;
+    }
+
 }
--- src/day20240522/p2/Test.java
+++ src/day20240522/p2/Test.java
@@ -3,5 +3,14 @@ package day20240522.p2;
 public class Test {
     public static void main(String[] args) {
         Entity a = new Entity("实训室8", "4台风扇");
+
+        Entity b = new Entity();
+        b.set("实训室3", "3台风扇");
+        b.get("实训室3");
+
+
+
+
+
     }
 }
