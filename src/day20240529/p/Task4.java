d380248c5ad0eb328823d0dd8177bd8c7d0e27f9
zxhhhxz1221 <1305894626@qq.com>
2024-05-29 21:19:59 +0800

    Task4 - 用树形的格式展示某个目录下的所有文件夹 - v1

--- src/day20240529/p/Task4.java
+++ src/day20240529/p/Task4.java
@@ -5,11 +5,11 @@ import java.io.File;
 public class Task4 {
     public static void main(String[] args) {
         File file = new File("src/day20240321");
-        String str = "--";
+        String str = "";
 
         // 用树形的格式展示某个目录下的所有文件夹
         // 每多一级，在文件名前面多两个减号
-
+        System.out.println(file.getName());
         printFileList(file, str);
 
 
@@ -17,13 +17,15 @@ public class Task4 {
     }
 
     private static void printFileList(File file, String str) {
+        str += "--";
         File[] files = file.listFiles();
         if (files != null)  {
             for (File f : files) {
-                if (file.isDirectory()) {
-                    printFileList(file,"--");
+                if (f.isDirectory()) {
+                    System.out.println(str + f.getName());
+                    printFileList(f,str);
                 } else {
-
+                    System.out.println(str + f.getName() + "\t字节：" + f.length() + "byte");
                 }
             }
         }
