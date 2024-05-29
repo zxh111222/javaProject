f8f6a87fd9f74f09f3ff27004cda0e17a8667ce2
zxhhhxz1221 <1305894626@qq.com>
2024-05-29 19:52:47 +0800

    Task3 - 统计代码仓库 src 目录下的文件类型和数量 - 基础代码

--- src/day20240529/p/Task3.java
+++ src/day20240529/p/Task3.java
@@ -1,5 +1,17 @@
 package day20240529.p;
 
-public class Task3 {
+import java.io.File;
 
+public class Task3 {
+    public static void main(String[] args) {
+        File rootDir = new File("src");
+        boolean isFalse = true;
+        while(true) {
+
+            isFalse = false;
+        }
+
+
+
+    }
 }
