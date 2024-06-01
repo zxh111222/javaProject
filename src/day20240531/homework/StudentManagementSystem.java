990234af36a2d59efa6658f85bbd011cbc6e7612
zxhhhxz1221 <1305894626@qq.com>
2024-06-01 15:16:18 +0800

    StudentManagementSystem -思路

---
+++ src/day20240531/homework/StudentManagementSystem.java
@@ -0,0 +1,25 @@
+package day20240531.homework;
+
+/**
+ * @author XinhaoZheng
+ * @version 1.0
+ * @description: StudentManagementSystem
+ * @date 2024/6/1 14:59
+ */
+
+/**
+ * 基于命令行的学生管理系统需求:
+ * 1. 列出所有学生信息
+ * 2. 增加学生                    - 增
+ * 3. 删除指定学生(根据姓名)        - 删
+ * 4. 更改指定学生信息(根据姓名)     - 改
+ * 5. 查找指定学生信息(根据姓名)     - 查
+ * 6. 退出系统
+ * 思路:
+ * 1. 需要主程序里面不停循环，当达到一个条件————输入`q`时退出
+ * 2. 需要一个 Student 类
+ * 3. 需要在主程序中调用不同的函数，实现学生管理系统中的需求
+ */
+
+public class StudentManagementSystem {
+}
