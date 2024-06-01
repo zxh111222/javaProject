dec9a4d1ae9ef1be8169e7f6945d7f0412dbede8
zxhhhxz1221 <1305894626@qq.com>
2024-06-01 14:35:47 +0800

    Collection - CollectionCommentMethod2 - remove 及 remove Overload 的相关面试题

---
+++ src/day20240531/p/CollectionCommentMethod3.java
@@ -0,0 +1,32 @@
+package day20240531.p;
+
+import java.util.ArrayList;
+
+public class CollectionCommentMethod3 {
+    public static void main(String[] args) {
+        ArrayList<String> als = new ArrayList<>();
+        als.add("abc");
+        als.add("qwe");
+        als.add("asd");
+        System.out.println(als.contains("asd"));
+
+        System.out.println("=== === ===");
+
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("X", 18));
+        students.add(new Student("Y", 19));
+        students.add(new Student("Z", 20));
+        System.out.println(students.contains(new Student("Y", 19)));
+
+    }
+}
+
+class Student {
+    String name;
+    int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
