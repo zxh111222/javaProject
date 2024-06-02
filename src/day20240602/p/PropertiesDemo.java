12d83bce47e03041e7b951132e51efa7b9ceb8d8
zxhhhxz1221 <1305894626@qq.com>
2024-06-02 17:10:20 +0800

    Collection - Map - Properties load 方法，从配置文件中获取 key 和 value

--- src/day20240602/p/PropertiesDemo.java
+++ src/day20240602/p/PropertiesDemo.java
@@ -1,5 +1,9 @@
 package day20240602.p;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 /**
@@ -9,11 +13,13 @@ import java.util.Properties;
  * @date 2024/6/2 17:00
  */
 public class PropertiesDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Properties properties = new Properties();
-        properties.put("username", "admin");
-        properties.put("password", "secret");
-        properties.put("token","ksadiadiowoiho3ioiasioads");
+//        properties.put("username", "admin");
+//        properties.put("password", "secret");
+//        properties.put("token","ksadiadiowoiho3ioiasioads");
+        File file = new File("src/day20240602/p/config.properties");
+        properties.load(new FileInputStream(file));
         System.out.println(properties);
         System.out.println(properties.getProperty("username"));
 
