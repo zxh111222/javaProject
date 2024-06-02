2c2e376301004e085dd4c160411a86fba0326aa4
zxhhhxz1221 <1305894626@qq.com>
2024-06-02 09:14:47 +0800

    Task3 - 完善 2024.05.29 c8285507 的代码

--- src/day20240529/p/Task3.java
+++ src/day20240529/p/Task3.java
@@ -5,42 +5,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Task3 {
+    private static final Map<String, Integer> FILE_TYPE_COUNTS = new HashMap<>();
+
     public static void main(String[] args) {
         File rootDir = new File("src");
+        countFileTypes(rootDir);
 
-        Map<String, Integer> fileTypeCount = new HashMap<>();
-
-        countFiles(rootDir, fileTypeCount);
-
-
-        for (Map.Entry<String, Integer> entry : fileTypeCount.entrySet()) {
+        for (Map.Entry<String, Integer> entry : FILE_TYPE_COUNTS.entrySet()) {
             System.out.println("类型: " + entry.getKey() + ", 数量: " + entry.getValue() + "个");
         }
     }
 
-    
-    private static void countFiles(File dirPath, Map<String, Integer> fileTypeCount) {
-        File[] files = dirPath.listFiles();
-        if (files != null)  {
+    private static void countFileTypes(File dir) {
+        File[] files = dir.listFiles();
+        if (files != null) {
             for (File file : files) {
-                if (file.isFile()) {
-                    String fileName = file.getName();
-                    int i = fileName.lastIndexOf('.');
-                    if (i > 0) {
-                        fileName = fileName.substring(i + 1);
-                    }else {
-                        fileName = "无后缀";
-                    }
-                    if (fileTypeCount.containsKey(fileName)) {
-                        fileTypeCount.put(fileName, fileTypeCount.get(fileName) + 1);
+                if (file.isDirectory()) {
+                    countFileTypes(file);
+                } else {
+                    String ext = getFileExtension(file.getName());
+                    if (FILE_TYPE_COUNTS.containsKey(ext)) {
+                        FILE_TYPE_COUNTS.put(ext, FILE_TYPE_COUNTS.get(ext) + 1);
                     } else {
-                        fileTypeCount.put(fileName, 1);
+                        FILE_TYPE_COUNTS.put(ext, 1);
                     }
-                } else if (file.isDirectory()) {
-                    countFiles(file, fileTypeCount);
                 }
             }
         }
+    }
+
+    private static String getFileExtension(String fileName) {
+        int lastDotIndex = fileName.lastIndexOf('.');
+        return lastDotIndex == -1 ? "无后缀" : fileName.substring(lastDotIndex + 1);
     }
 }
     
