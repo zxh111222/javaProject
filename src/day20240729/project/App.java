aaa947f5ca6591b0614e7444f7b6e567779e60ee
zxhhhxz1221 <1305894626@qq.com>
2024-07-29 11:25:24 +0800

    Project - 以 Boss 的视角规划整体框架

---
+++ src/day20240729/project/App.java
@@ -0,0 +1,31 @@
+package day20240729.project;
+
+/**
+ * @author XinhaoZheng
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/7/29 11:21
+ */
+public class App {
+    public static void main(String[] args) {
+        // 读取配置文件
+        System.out.println("配置文件读取成功!");
+
+        // 下载模块 Downloader :
+
+
+        // 解析模块 Parser :
+
+
+        // 存储模块 Storage :
+
+
+        // 通知模块 Notificator :
+
+
+
+
+
+    }
+
+}
