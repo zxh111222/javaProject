3bcd246a8a76e248248a94ae14cf421b1c573bb2
zxhhhxz1221 <1305894626@qq.com>
2024-07-29 16:55:09 +0800

    阶段项目 improvement-3 - 将邮件发送独立出来为一个工具类

--- src/day20240729/phase_project/notificator/EmailNotificator.java
+++ src/day20240729/phase_project/notificator/EmailNotificator.java
@@ -1,14 +1,11 @@
 package day20240729.phase_project.notificator;
 
 
-import jakarta.mail.*;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeBodyPart;
-import jakarta.mail.internet.MimeMessage;
-import jakarta.mail.internet.MimeMultipart;
+import day20240729.phase_project.util.MyEmailUtil;
 
 import java.util.List;
 import java.util.Properties;
+
 
 /**
  * @author XinhaoZheng
@@ -50,43 +47,10 @@ public class EmailNotificator implements Notificator {
             }
         }
         //System.out.println(content);
-        send(sender, secret_token, receiver, String.valueOf(content));
+
+        MyEmailUtil.send(sender, secret_token, receiver, String.valueOf(content));
     }
 
-    public static void send(String sender, String secret_token, String receiver, String content)  throws Exception {
-        Properties prop = new Properties();
-        prop.put("mail.smtp.auth", true);
-        prop.put("mail.smtp.starttls.enable", "true");
-        prop.put("mail.smtp.host", "smtp.qq.com");
-        prop.put("mail.smtp.port", "587");
 
-        // https://wx.mail.qq.com/list/readtemplate?name=app_intro.html#/agreement/authorizationCode
-        Session session = Session.getInstance(prop, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(sender, secret_token);
-            }
-        });
-
-        Message message = new MimeMessage(session);
-        // who you are
-        message.setFrom(new InternetAddress(sender));
-        // send to ...
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiver));
-
-        message.setSubject("来自 Java 的提醒");
-
-        MimeBodyPart mimeBodyPart = new MimeBodyPart();
-        mimeBodyPart.setContent(content, "text/html; charset=utf-8");
-
-        Multipart multipart = new MimeMultipart();
-        multipart.addBodyPart(mimeBodyPart);
-
-        message.setContent(multipart);
-
-        Transport.send(message);
-
-        System.out.println("邮件发送成功");
-    }
 
 }
