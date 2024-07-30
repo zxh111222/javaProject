344cc280265ac8618a6d388557b49532f85085e5
zxhhhxz1221 <1305894626@qq.com>
2024-07-30 16:50:52 +0800

    enum - values()

---
+++ src/day20240730/enum_demo2/CustomType.java
@@ -0,0 +1,12 @@
+package day20240730.enum_demo2;
+
+/**
+ * @author XinhaoZheng
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/7/30 16:48
+ */
+public enum CustomType {
+    JAVA,
+    PYTHON
+}
---
+++ src/day20240730/enum_demo2/CustomTypeTest.java
@@ -0,0 +1,21 @@
+package day20240730.enum_demo2;
+
+/**
+ * @author XinhaoZheng
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/7/30 16:48
+ */
+public class CustomTypeTest {
+    public static void main(String[] args) {
+        System.out.println(CustomType.JAVA);
+        System.out.println(CustomType.PYTHON);
+
+        CustomType[] values = CustomType.values();
+        for (CustomType value : values) {
+            System.out.println(value);
+        }
+    }
+
+
+}
