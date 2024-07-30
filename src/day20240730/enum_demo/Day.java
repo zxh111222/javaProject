204a45276c848bf6c954b8e8515231141c499770
zxhhhxz1221 <1305894626@qq.com>
2024-07-30 15:26:58 +0800

    enum - 枚举类型

---
+++ src/day20240730/enum_demo/Day.java
@@ -0,0 +1,12 @@
+package day20240730.enum_demo;
+
+
+public enum Day {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
---
+++ src/day20240730/enum_demo/DayTest.java
@@ -0,0 +1,14 @@
+package day20240730.enum_demo;
+
+/**
+ * @author XinhaoZheng
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/7/30 14:19
+ */
+public class DayTest {
+    public static void main(String[] args) {
+        Day monday = Day.MONDAY;
+        System.out.println(Day.MONDAY);
+    }
+}
