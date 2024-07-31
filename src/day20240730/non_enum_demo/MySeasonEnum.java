f6f342d3ea6ce3b3a0abd9348de81b189a7d21db
zxhhhxz1221 <1305894626@qq.com>
2024-07-31 21:17:31 +0800

    enum - 自定义枚举类(含细节) - 季节

---
+++ src/day20240730/non_enum_demo/MySeasonEnum.java
@@ -0,0 +1,41 @@
+package day20240730.non_enum_demo;
+
+/**
+ * @author XinhaoZheng
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/7/31 20:24
+ */
+public class MySeasonEnum {
+    private String name;
+    private String characteristic; // 特点
+
+    public static final MySeasonEnum SPRING = new MySeasonEnum("春天", "鸟语花香");
+    public static final MySeasonEnum SUMMER = new MySeasonEnum("夏天", "烈日炎炎");
+    public static final MySeasonEnum AUTUMN = new MySeasonEnum("秋天", "秋高气爽");
+    public static final MySeasonEnum WINTER = new MySeasonEnum("冬天", "鹅毛大雪");
+
+
+    // 构造器私有化 -> 这样外界就不能直接创建对象，防止直接 new
+    private MySeasonEnum(String name, String characteristic) {
+        this.name = name;
+        this.characteristic = characteristic;
+    }
+
+
+    // 只提供 get 方法 不提供 set 方法 -> 只读不写
+    public String getName() {
+        return name;
+    }
+    public String getCharacteristic() {
+        return characteristic;
+    }
+
+    @Override
+    public String toString() {
+        return "MySeasonEnum{" +
+                "name='" + name + '\'' +
+                ", characteristic='" + characteristic + '\'' +
+                '}';
+    }
+}
