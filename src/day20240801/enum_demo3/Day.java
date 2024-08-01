f2d7ab2f0efe788b356139bef9094d9c42ba61eb
zxhhhxz1221 <1305894626@qq.com>
2024-08-01 14:30:23 +0800

    enum - 新需求: 让每个 Day 的对象，有各自的描述 desc()...
    可以在枚举内的具体每个对象内部实现接口定义的方法

--- src/day20240801/enum_demo3/Day.java
+++ src/day20240801/enum_demo3/Day.java
@@ -2,16 +2,46 @@ package day20240801.enum_demo3;
 
 
 public enum Day implements ForEnumDemo {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY;
-
-    @Override
-    public void desc() {
-        System.out.println("Day@desc");
-    }
+    MONDAY {
+        @Override
+        public void desc() {
+            System.out.println("星期一: 一脸懵逼");
+        }
+    },
+    TUESDAY {
+        @Override
+        public void desc() {
+            System.out.println("星期二: 前不着村，后不着店");
+        }
+    },
+    WEDNESDAY {
+        @Override
+        public void desc() {
+            System.out.println("星期三: Midweek days are so so");
+        }
+    },
+    THURSDAY {
+        @Override
+        public void desc() {
+            System.out.println("星期四: 加把劲");
+        }
+    },
+    FRIDAY {
+        @Override
+        public void desc() {
+            System.out.println("星期五: 心已放假");
+        }
+    },
+    SATURDAY {
+        @Override
+        public void desc() {
+            System.out.println("星期六: 噢啊");
+        }
+    },
+    SUNDAY {
+        @Override
+        public void desc() {
+            System.out.println("星期天: 哦草，就这...");
+        }
+    };
 }
